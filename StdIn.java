40e24e12153e073a976ac778b37e274940be8d53
escray <zescray@gmail.com>
2015-09-21 18:52:36 +0800

    bag and queue

---
+++ StdIn.java
@@ -0,0 +1,23 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Queue;
+
+public class StdIn
+{
+  public static int[] readInts(String name)
+  {
+    In in = new In(name);
+    Queue<Integer> q = new Queue<Integer>();
+    while (!in.isEmpty())
+    {
+      q.enqueue(in.readInt());
+    }
+
+    int N = q.size();
+    int[] a = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+      a[i] = q.dequeue();
+    }
+    return a;
+  }
+}
---
+++ TwoSumFast.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.In;
+
+public class TwoSumFast
+{
+  public static int count(int[] a)
+  {
+    Arrays.sort(a);
+    int N = a.length;
+    int cnt = 0;
+    for (int i = 0; i < N; i++)
+    {
+      if (BinarySearch.rank(-a[i], a) > i)
+      {
+        cnt++;
+      }
+    }
+    return cnt;
+  }
+
+  public static void main(String[] args)
+  {
+    In in = new In(args[0]);
+    int[] a = in.readAllInts();
+    StdOut.println(count(a));
+  }
+}
